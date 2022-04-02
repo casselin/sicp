a42bbd41885847dc61e462a7e6d131fa3bf367cd
Craig Asselin <asselin.craig@gmail.com>
2022-04-02 11:32:10 -0400

    Add Exercises 1.11 - 1.13

--- c1/1-2.rkt
+++ c1/1-2.rkt
@@ -74,3 +74,49 @@ Thus g(n) = 2^n
 There will be m = (h (- n 1)) factors of 2 in the product
 Therefore (h n) = 2^(h (- n 1))
 |#
+
+;; Exercise 1.11
+(define (f-rec n)
+  (cond ((< n 3) n)
+        (else (+ (f-rec (- n 1))
+                 (* 2 (f-rec (- n 2)))
+                 (* 3 (f-rec (- n 3)))))))
+
+(define (f-iter n)
+  (define (iter a b c count)
+    (if (= count 0)
+        c
+        (iter (+ a (* 2 b) (* 3 c))
+              a
+              b
+              (- count 1))))
+  (iter 2 1 0 n))
+
+;; Exercise 1.12
+(define (pascal r c)
+  (cond ((or (< r 0) (< c 0)) 0)
+        ((= r 0) 1)
+        ((or (= c 0) (= c r)) 1)
+        (else (+ (pascal (- r 1) (- c 1))
+                 (pascal (- r 1) c)))))
+
+;; Exercise 1.13
+#|
+Let \phi = (1 + sqrt(5))/2 and \psi = (1 - sqrt(5))/2
+Will show that Fib(n) = (\phi^n - \psi^n)/sqrt(5) by strong induction:
+Base case:
+Fib(0) = (1 - 1)/sqrt(5) = 0
+Fib(1) = (1+sqrt(5)/2 - 1+sqrt(5)/2)/sqrt(5) = 1
+
+Inductive case:
+Fib(n+1) = Fib(n) + Fib(n-1)
+         = (\phi^n - \psi^n)/sqrt(5) + (\phi^(n-1) - \psi^(n-1))/sqrt(5)
+         = (\phi^(n-1)(\phi + 1) - \psi^(n-1)(\psi + 1))/sqrt(5)
+         = (\phi^(n-1)(\phi^2) - \psi^(n-1)(\psi^2))/sqrt(5)
+         = (\phi^(n+1) - \psi^(n+1))/sqrt(5)
+Used the fact that \phi^2 = \phi + 1 and \psi^2 = \psi + 1
+
+We have that \psi < 1, and as n -> \inf \psi^n -> 0. Thus \psi^n is
+insignificant in the closed formula of Fib(n), and we may simply
+compute \phi^n/sqrt(5) and round to the nearest integer.
+|#

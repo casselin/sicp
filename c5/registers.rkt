61fc83c8f5a058e00ebc4972dbe1bb3c7321f752
Craig Asselin <asselin.craig@gmail.com>
2023-11-07 22:14:53 -0500

    Add up to exercise 5.13

--- c5/registers.rkt
+++ c5/registers.rkt
@@ -134,11 +134,14 @@ value can be left on the stack unchanged.
 |#
 
 ;;; A Resgister-Machine Simulator
-(define (make-machine register-names ops controller-text)
+; by exercise 5.13 make-machine no longer needs a list of register names
+(define (make-machine ops controller-text)
   (let ((machine (make-new-machine)))
+    #|
     (for-each (lambda (register-name)
                 ((machine 'allocate-register) register-name))
               register-names)
+    |#
     ((machine 'install-operations) ops)
     ((machine 'install-instruction-sequence)
      (assemble controller-text machine))
@@ -214,11 +217,15 @@ value can be left on the stack unchanged.
                   (cons (list name (make-register name))
                         register-table)))
         'register-allocated)
+      ;; exercise 5.13
       (define (lookup-register name)
         (let ((val (assoc name register-table)))
           (if val
               (cadr val)
-              (error "Unknown register: " name))))
+              (begin
+                (allocate-register name)
+                (lookup-register name)))))
+      ;
       (define (execute)
         (let ((insts (get-contents pc)))
           (if (null? insts)
@@ -696,40 +703,35 @@ let (...
       ((machine 'install-register-sources)
        (extract-reg-sources (if assign-list assign-list '()))))))
 
-(define fib-machine
-  (make-machine
-   '(n continue val)
-   (list (list '< <)
-         (list '- -)
-         (list '+ +))
-   '(controller
-       (assign continue (label fib-done))
-     fib-loop
-       (test (op <) (reg n) (const 2))
-       (branch (label immediate-answer))
-       ;; set up to compute Fib(n-1)
-       (save continue)
-       (assign continue (label afterfib-n-1))
-       (save n)                            ;save old value of n
-       (assign n (op -) (reg n) (const 1)) ;clobber n to n-1
-       (goto (label fib-loop))             ;perform recursive call
-     afterfib-n-1                          ;upon return, val contains Fib(n-1)
-       (restore n)
-       (restore continue)
-       ;; set up to compute Fib(n-2)
-       (assign n (op -) (reg n) (const 2))
-       (save continue)
-       (assign continue (label afterfib-n-2))
-       (save val)                          ;save Fib(n-1)
-       (goto (label fib-loop))
-     afterfib-n-2                          ;upon return, val contains Fib(n-2)
-       (assign n (reg val))                ;n now contains Fib(n-2)
-       (restore val)                       ;val now contains Fib(n-1)
-       (restore continue)
-       (assign val                         ;Fib(n-1) + Fib(n-2)
-               (op +) (reg val) (reg n))
-       (goto (reg continue))               ;return to caller, answer is in val
-     immediate-answer
-       (assign val (reg n))                ;base case: Fib(n) = n
-       (goto (reg continue))
-     fib-done)))
+(define fib-machine-controller
+  '(controller
+    (assign continue (label fib-done))
+    fib-loop
+      (test (op <) (reg n) (const 2))
+      (branch (label immediate-answer))
+      ;; set up to compute Fib(n-1)
+      (save continue)
+      (assign continue (label afterfib-n-1))
+      (save n)                            ;save old value of n
+      (assign n (op -) (reg n) (const 1)) ;clobber n to n-1
+      (goto (label fib-loop))             ;perform recursive call
+    afterfib-n-1                          ;upon return, val contains Fib(n-1)
+      (restore n)
+      (restore continue)
+      ;; set up to compute Fib(n-2)
+      (assign n (op -) (reg n) (const 2))
+      (save continue)
+      (assign continue (label afterfib-n-2))
+      (save val)                          ;save Fib(n-1)
+      (goto (label fib-loop))
+    afterfib-n-2                          ;upon return, val contains Fib(n-2)
+      (assign n (reg val))                ;n now contains Fib(n-2)
+      (restore val)                       ;val now contains Fib(n-1)
+      (restore continue)
+      (assign val                         ;Fib(n-1) + Fib(n-2)
+              (op +) (reg val) (reg n))
+      (goto (reg continue))               ;return to caller, answer is in val
+    immediate-answer
+      (assign val (reg n))                ;base case: Fib(n) = n
+      (goto (reg continue))
+    fib-done))
